b318605ffdb586b174b568c47ea1d0b8f5e9fe1c
kyang12 <kyang12@ncsu.edu>
2017-09-23 16:42:29 -0400

    add admin login feature in sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,12 @@ class SessionsController < ApplicationController
   def create
     user_type = params[:session][:user_type].to_i
     @user = Customer.find_by email: params[:session][:email] if user_type == 0
-    @user = Admin.find_by email: params[:session][:email] if user_type == 1
+    @user = Admin.find_by email: params[:session][:email] if (user_type == 1 || user_type==2)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_type] = user_type
       session[:user_id] = @user.id
       redirect_to customer_path(@user) if 0 == user_type
+      redirect_to admin_path(@user) if (1==user_type)||(2==user_type)
     else
       redirect_to login_path
     end
