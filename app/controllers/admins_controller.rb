e441e32d0c372083e010c19f0d4db4719dc36d85
kyang12 <kyang12@ncsu.edu>
2017-09-23 20:24:43 -0400

    revise the view of admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,6 @@
 class AdminsController < ApplicationController
   before_action :set_admin, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:index, :show, :edit, :update, :destroy]
 
   # GET /admins
   def index
