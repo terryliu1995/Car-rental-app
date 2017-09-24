65bb19f46210b73af550dc23322e4ea420bbb8f6
kyang12 <kyang12@ncsu.edu>
2017-09-24 19:50:31 -0400

    add default SuperAdmin in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create(email: 'root@ncsu', name: 'root', password: 'root', issuper: 2)
