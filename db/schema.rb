00ec913163cccae3c9f403acfd0c0321d14477f3
kyang12 <kyang12@ncsu.edu>
2017-09-25 17:56:44 -0400

    Remove endTime

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922222556) do
+ActiveRecord::Schema.define(version: 20170925214646) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20170922222556) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "status"
-    t.datetime "endTime"
     t.integer  "customer_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
