6863b34cc6b9deba953f6703cb9fe19cd8eac26d
kyang12 <kyang12@ncsu.edu>
2017-10-03 13:50:17 -0400

    move car logic into model

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -14,7 +14,7 @@ class CarsController < ApplicationController
     elsif @task == 0 # customer reserve car  task = 2
       params[:sstatus] ||= 0
       @customer = current_user
-    elsif 2 == @task
+    elsif @task == 2
       params[:sstatus] ||= '-1'
       @car_status_default = -1
     end
@@ -29,52 +29,8 @@ class CarsController < ApplicationController
 
   # GET /cars/1
   def show
-    @url = {}
-    user_type = session[:user_type]
-    if params[:task]
-      task = params[:task].to_i
-    elsif 0 == user_type
-      task = 0
-    elsif [1, 2].include? user_type
-      task = 2
-    end
-    car_id = @car.id
-
-    @url[:back] = "#{cars_path}?task=#{task}"
-    if user_type == 0
-      @customer = current_user
-      customer_reservation = @customer.current_reservation
-      if customer_reservation
-        if @car.id == customer_reservation.car.id
-          if @car.status == 2
-            @url[:checkout] = "#{checkout_path}?car_id=#{car_id}"
-          elsif @car.status == 1
-            @url[:checkin] = "#{checkin_path}?task=#{task}&car_id=#{car_id}"
-          end
-        end
-      elsif @car.status == 0
-        @url[:reserve] = "#{new_reservation_path}?task=#{task}&car_id=#{car_id}"
-      end
-    elsif [1, 2].include?(user_type)
-      if task == 1 # on behalf of user to rent car
-        @customer = Customer.find(params[:customer_id].to_i)
-        @url[:reserve] = "#{new_reservation_path}?task=#{task}&car_id=#{car_id}" if 0 == @car.status
-      else
-        @url[:history] = "#{reservations_path}?task=2&car_id=#{car_id}"
-        if @car.status == 2
-          @url[:checkout] = "#{checkout_path}?car_id=#{car_id}"
-        elsif @car.status == 1
-          @url[:checkin] = "#{checkin_path}?task=#{task}&car_id=#{car_id}"
-        end
-      end
-    end
-
-    if @customer
-      customer_id = @customer.id
-      @url[:reserve] += "&customer_id=#{customer_id}" if @url[:reserve]
-      @url[:checkout] += "&customer_id=#{customer_id}" if @url[:checkout]
-      @url[:back] += "&customer_id=#{customer_id}" if @url[:back]
-    end
+    @url_params = @car.availalbe_params(session[:user_type],
+                                        params[:task], params[:customer_id])
   end
 
   # GET /cars/new
